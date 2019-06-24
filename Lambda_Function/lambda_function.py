0b7485f694fc7586717e572b242b7a7328e8764a
Lloyd Matereke <lloydmatereke23@gmail.com>
2019-06-24 18:36:39 +0200

    Implemented Lambda function

--- Lambda_Function/lambda_function.py
+++ Lambda_Function/lambda_function.py
@@ -1,2 +1,22 @@
-#TODO
-#Implement lambda_handler
+import json
+import boto3
+
+
+GLUE_ETL_JOB_NAME = "<Enter the name of the Glue ETL Job name>"
+
+def lambda_handler(event, context):
+    """
+    Method is used to invoke Glue ETL job.
+    
+    Args:
+        event (dict): Data passed from Amazon S3 PUT event.
+        context (LambdaContext): Contains methods and properties
+        that provide information about the invocation, function, and execution environment. 
+        
+    Returns:
+        dict: Response from triggering Glue ETL Job.
+    """
+    client = boto3.client('glue')
+    response = client.start_job_run(
+        JobName=GLUE_ETL_JOB_NAME)
+    return response
